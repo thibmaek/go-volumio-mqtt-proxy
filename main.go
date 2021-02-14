1655a83ade2cf3ddc68ca7b59203c3cf54511652
thibmaek <thibault.maekelbergh@icloud.com>
2021-02-14 18:04:51 +0100

    🎨 Keep the connection open and tick MPC command

--- main.go
+++ main.go
@@ -1,26 +1,68 @@
 package main
 
-import "fmt"
-
-const clientId = "go-volumio-mqtt"
-
-var (
-	broker   string
-	user     string
-	password string
-	port     = 1883
+import (
+	"fmt"
+	volumio "go-volumio-mqtt/volumio"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func listen(c *Client) {
 	res, err := c.Subscribe("test/topic")
 
 	for update := range res {
-		fmt.Printf("Received update: %s", update)
+		fmt.Printf("Received update: %s\n", update)
 	}
 
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+// Keeps the connection upon until signal is interrupted or terminated
+func keepOpen() chan bool {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Printf("\n%s\n", sig)
+		done <- true
+	}()
+	return done
+}
+
+func updateTrack(client *Client) *time.Ticker {
+	ticker := time.NewTicker(time.Second * 1)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				track, err := volumio.GetCurrentTrack()
+				if err != nil {
+					fmt.Printf("Failed reading current track: %v", err)
+				}
+
+				err = client.Publish("track", Message{
+					payload: track,
+					retain:  false,
+				})
+				if err != nil {
+					fmt.Printf("Error publishing current track: %v", err)
+				}
+			}
+		}
+	}()
+
+	return ticker
 }
 
 func main() {
@@ -34,6 +76,8 @@ func main() {
 
 	go listen(client)
 
-	for {
-	}
+	ticker := updateTrack(client)
+	defer ticker.Stop()
+
+	<-keepOpen()
 }
